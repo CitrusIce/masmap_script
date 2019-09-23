3f7ebf19ec83bfb0a887122b10929d39398ddd17
me <me@example.com>
2019-09-23 16:36:57 +0800

    finish mas scanner

--- masmap_scpript.py
+++ masmap_scpript.py
@@ -1,25 +1,29 @@
+# -*- coding: utf-8 -*-
 #  首先运行2或3个并发的Masscan任务，所有的65535个端口分为4-5个更小的范围；
 #  获取主机列表以及Masscan扫描出的开放端口的组合列表；
 #  使用这些列表作为Nmap的扫描目标并执行常规Nmap扫描。
 
 import subprocess
+import pysnooper
 
 class task:
-    def __init__(self,ip,ports):
+    def __init__(self,ip,ports_list):
          self.ip=ip
-         self.ports=ports
+         self.ports=','.join([str(x) for x in ports_list])
 
 class Mas_Scanner:
     def __init__(self,task_list):
         self.task_list=task_list
         self.result={}
     
+    # @pysnooper.snoop()
     def analyze(self,data,ip):#analyze and add task scan result to scanner result
         ports_list=[]
         for line in data.split('\n'):
-            port = line.split()[3] # xx/tcp
-            port = port.split('/')[0] 
-            ports_list.append(port)
+            if line!='':
+                port = line.split()[3] # xx/tcp
+                port = port.split('/')[0] 
+                ports_list.append(port)
 
         if ip in self.result:
             self.result[ip]=self.result[ip]+ports_list
@@ -27,11 +31,11 @@ class Mas_Scanner:
 
     def start_task(self,task): #one task scan only one ip
         cmd = 'masscan {ip} -p{ports} --rate 50000'.format(ip=task.ip,ports=task.ports)
-        stdoutdata = subprocess.getoutput(cmd)
+        print(cmd)
+        proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        stdoutdata=proc.stdout.read().decode('UTF-8')
         self.analyze(stdoutdata,task.ip)
     
-
-
     def run(self):
         for task in self.task_list:
             self.start_task(task)
@@ -39,6 +43,8 @@ class Mas_Scanner:
 
 
 if __name__=='__main__':
-    pass
-    # scanner=Mas_Scanner(1,2)
+    task_ = task('47.93.234.29',list(range(1,1000)))
+    scanner=Mas_Scanner([task_,])
+    scanner.run()
+    print(scanner.result)
     # scanner.run()
