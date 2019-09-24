ee5131ea356a1add7b71a731dd6ebd1d83cbbd93
me <me@example.com>
2019-09-24 23:00:49 +0800

    combine two scanner

--- masmap_scpript.py
+++ masmap_scpript.py
@@ -5,19 +5,23 @@
 
 import subprocess
 import pysnooper
+import threading
+import os
 import nmap
 
 class Task:
     def __init__(self,ip,ports_list):
          self.ip=ip
+         ports_list.sort()
          self.ports=','.join([str(x) for x in ports_list])
 
-class Mas_Scanner:  
+class Mas_Scanner(threading.Thread):  
     def __init__(self,task_list):
+        threading.Thread.__init__(self)
         self.task_list=task_list
         self.result={}
     
-    # @pysnooper.snoop()
+    @pysnooper.snoop()
     def analyze(self,data,ip):#analyze and add task scan result to scanner result
         ports_list=[]
         for line in data.split('\n'):
@@ -28,13 +32,21 @@ class Mas_Scanner:
 
         if ip in self.result:
             self.result[ip]=self.result[ip]+ports_list
-        self.result[ip]=ports_list
+        else:
+            self.result[ip]=ports_list
+        print(self.result[ip])
+        print(ports_list)
 
+    @pysnooper.snoop()
     def start_task(self,task): #one task scan only one ip with different ports
-        cmd = 'masscan {ip} -p{ports} --rate 50000'.format(ip=task.ip,ports=task.ports)
-        print(cmd)
+        # cmd = 'masscan {ip} -p{ports} --rate 50000 --wait 0'.format(ip=task.ip,ports=task.ports)
+        cmd = 'masscan {ip} -p{ports} --wait 0 --rate 1500'.format(ip=task.ip,ports=task.ports)
+        # print(cmd)
         proc = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        proc.wait()
         stdoutdata=proc.stdout.read().decode('UTF-8')
+        stderrdata = proc.stderr.read().decode('UTF-8')
+        print(stdoutdata)
         self.analyze(stdoutdata,task.ip)
     
     def run(self):
@@ -59,7 +71,8 @@ class Nmap_Scanner:
             print('----------------------------------------------------')
             print('Host : {}'.format(ip))
             print('--------------------')
-            for port in self.result[ip]['tcp']:
+            ports_dict = sorted(self.result[ip]['tcp'])
+            for port in ports_dict:
                 portinfo=self.result[ip]['tcp'][port]
                 str_buffer = 'port : {port}\tstate : {state}\t{name} {product} {version}'.format(port=port,
                                                                                                state=portinfo['state'],
@@ -71,12 +84,80 @@ class Nmap_Scanner:
         for task in self.task_list:
             self.start_task(task)
 
+# @pysnooper.snoop()
+def split_task(ip): #split ports range in 5 pieces and return a list
+    ports_list = [str(x) for x in range(1,49152)]
+    n = int(65535/5)
+    task_list = []
+    for x in range(5):
+        list_ = ports_list[n*x:n*x+n]
+        task = Task(ip,list_)
+        task_list.append(task)
+    
+    return task_list
+
+    
+
+
+
+
+@pysnooper.snoop()
+def main():
+    # print([x.ports for x in split_task('47.93.234.29')])
+    if os.getuid() != 0:
+        print('please run this script with root!')
+        exit()
+    task_list = split_task('47.93.234.29')
+    masscaner_num = 2
+    task_per_scanner = int(len(task_list)/masscaner_num)
+    masscanner_list = []
+    # x = 0
+    # scanner = Mas_Scanner([Task('47.93.234.29',[22,]),])
+    # scanner = Mas_Scanner(task_list[x*task_per_scanner:x*task_per_scanner+task_per_scanner])
+    # scanner.run()
+    # print(scanner.result)
+    # exit()
+    for x in range(masscaner_num):
+        scanner = Mas_Scanner(task_list[x*task_per_scanner:x*task_per_scanner+task_per_scanner])
+        masscanner_list.append(scanner)
+        scanner.start()
+
+    for scanner in masscanner_list:
+        scanner.join()
+        
+    
+    masscan_result = {}
+    for scanner in masscanner_list:
+        for ip in scanner.result:
+            if ip not in masscan_result:
+                masscan_result[ip]=scanner.result[ip]
+            else:
+                masscan_result[ip]+=scanner.result[ip]
+
+    print(masscan_result)
+    nmap_task_list = []
+    for ip in masscan_result:
+        nmap_task_list.append(Task(ip,masscan_result[ip]))
+
+    nm_scanner = Nmap_Scanner(nmap_task_list)
+    nm_scanner.run()
+    nm_scanner.print_scan_result()
+
 
 if __name__=='__main__':
-    task_ = Task('47.93.234.29',[22,4444])
-    nm = Nmap_Scanner([task_,])
-    nm.run()
-    nm.print_scan_result()
+    print(Task('47.',[6600,22]).ports)
+    nm_scanner = Nmap_Scanner([Task('47.93.234.29',[6600,22])])
+    nm_scanner.run()
+    nm_scanner.print_scan_result()
+    # main()
+
+
+
+
+    # task_ = Task('47.93.234.29',[22,4444])
+    # nm = Nmap_Scanner([task_,])
+    # nm.run()
+    # nm.print_scan_result()
     #  scanner=Mas_Scanner([task_,])
     #  scanner.run()
     #  print(scanner.result)
